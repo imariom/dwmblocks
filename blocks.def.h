2340ab8612f73f4a4ff308cc66447d29328c1262
imariom <connect@imariom.com>
2024-09-29 23:23:49 +0200

    setup initial imOS status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/		/*Update Signal*/
-	{"", 		"upddates",		60,						0},
-	{"", 		"memory",		5,						0},
-	{"", 		"forecast",		60,						0},
-	{"", 		"nettraf",		5,						0},
-	{"", 		"internet",		5,						0},
+	{"", 		"cpu",			10,						0},
+	{"", 		"memory",		10,						0},
+	{"", 		"nettraf",		10,						0},
+	{"", 		"updates",		60,						0},
+	{"", 		"internet",		10,						0},
 	{"", 		"volume",		5,						0},
-	{"", 		"clock",		10,						0},
+	{"", 		"clock",		60,						0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
